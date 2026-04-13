21eb2e964108ccc26529873d1153c28c3a805180
Ahmad Abubakar <ahmadabk6906@gmail.com>
2026-04-13 11:37:41 -0100

    Add Login API

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from.views import *
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 routers = DefaultRouter()
@@ -9,6 +10,7 @@ routers.register('tasks', TaskViewSet)
 
 urlpatterns = [
     path("register/", RegisterView.as_view())
+    path('login/', obtain_auth_token)
     # path('tasks/', TaskList.as_view()),
     # path('tasks/<int:pk>/', TaskDetails.as_view())
 ]

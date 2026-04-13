499be1f5d0177c5bd2d57ef8719cca65d1121a6e
Ahmad Abubakar <ahmadabk6906@gmail.com>
2026-04-13 11:45:15 -0100

    Add Login API in a ROOT-URL again

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('TodoApp.urls')),
-    path('login/', obtain_auth_token)
+    path('login/', obtain_auth_token),
 
 ]

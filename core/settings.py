47f78e1ae1fe97ea6d4066f1e72aad19eb39f1c7
Ahmad Abubakar <ahmadabk6906@gmail.com>
2026-04-14 20:48:27 -0100

    add postresql

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,14 +87,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+DATABASE_URL=postgresql://todo_db_b7hf_user:P9y5JHGz2eNE1EafwtCuMyPOPDzTh1Pz@dpg-d7f9bj57vvec73a85pbg-a/todo_db_b7hf
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 

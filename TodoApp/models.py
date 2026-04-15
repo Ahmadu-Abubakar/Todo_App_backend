9fe0dd75fab3a6c1c124ffe371f27dfd86a0f87e
Ahmad Abubakar <ahmadabk6906@gmail.com>
2026-04-15 04:21:23 -0100

    adding multi-user isolation

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -3,11 +3,6 @@ from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 from django.contrib.auth.models import User
 
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
-
-
 class Task(models.Model):
     user = models.ForeignKey(User,  on_delete=models.CASCADE)
     task = models.CharField(max_length=100)
--- TodoApp/views.py
+++ TodoApp/views.py
@@ -17,9 +17,11 @@ class RegisterView(CreateAPIView):
     serializer_class = RegisterSerializer
 
 class TaskViewSet(ModelViewSet):
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)

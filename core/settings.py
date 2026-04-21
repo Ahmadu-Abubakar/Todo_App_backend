374b3a28b4bccdb5ce68e82958d3bf9fa8cff70c
Ahmad Abubakar <ahmadabk6906@gmail.com>
2026-04-21 06:57:24 -0100

    Adding JWT system

--- core/settings.py
+++ core/settings.py
@@ -63,6 +63,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3  # you can change this
--- core/urls.py
+++ core/urls.py
@@ -18,10 +18,17 @@ from django.contrib import admin
 from django.urls import path, include
 from TodoApp import urls
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('TodoApp.urls')),
     path('login/', obtain_auth_token),
-    path("api-auth/", include("rest_framework.urls")),  # 👈 ADD THIS
+    path("api-auth/", include("rest_framework.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

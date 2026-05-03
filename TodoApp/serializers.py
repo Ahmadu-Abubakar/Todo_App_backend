2a3837ab19b7f04776b2df2ff5ed23fe84e65fe2
Ahmad Abubakar <ahmadabk6906@gmail.com>
2026-05-03 04:39:33 -0100

    adding a nice time format step2

--- TodoApp/serializers.py
+++ TodoApp/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 from .models import Task
+from django import forms
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = "__all__"
         read_only_fields = ['user']
+        widgets = {
+            'time_created': forms.TimeInput(format='%H:%M'),
+        }
+
 
 
 from django.contrib.auth.models import User
